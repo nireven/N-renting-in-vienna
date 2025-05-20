3a7c846b8d0825aa5618cbf22bf753d50a168115
nireven <62841300+nireven@users.noreply.github.com>
2025-05-20 16:23:27 +0200

    Update filter_datatset.py

--- filter_datatset.py
+++ filter_datatset.py
@@ -13,10 +13,10 @@ now = pd.Timestamp.now()
 one_day_ago = now.tz_localize('UTC') - pd.Timedelta(days=1)
 
 # Change the districts that you care about here
-allowed_districts = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17}
+allowed_districts = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 14, 15, 16, 17,18,19,20,21,22,23}
 
 filtered_df = df[(df.State == 'Wien') & 
-                 (df.Price < 800) &
+                 (df.Price < 900) &
                  (df.Price > 400) &
                  (df.Rooms > 1) &
                  (df['Property Type'] == 'Wohnung') &
